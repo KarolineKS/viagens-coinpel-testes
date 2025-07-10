bb369f0d177e17dc04695949bc62fa2a2adc3f3a
KarolineKS <kkohlsstangherlin@gmail.com>
2025-07-10 10:37:19 -0300

    feat(assets): configurar processamento de imagens com vite

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,12 @@
 
 @section('content')
 
+<style>
+    :root {
+        --img-login-bg: url('{{ asset(' images/img-login.jpg') }}');
+        --icon-close: url('{{ asset(' images/x.svg') }}');
+    }
+</style>
 
 <div class="auth-layout">
     <div class="auth-layout__form-section">
--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,18 @@ export default defineConfig({
             "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
         },
     },
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    const info = assetInfo.name.split(".");
+                    const extType = info[info.length - 1];
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        return `images/[name].[hash][extname]`;
+                    }
+                    return `assets/[name].[hash][extname]`;
+                },
+            },
+        },
+    },
 });
