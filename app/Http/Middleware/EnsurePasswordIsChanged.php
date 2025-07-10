7311a79c3488ffee3c37d733e9069d0802d8f8d3
KarolineKS <kkohlsstangherlin@gmail.com>
2025-07-10 11:32:22 -0300

    refactor(auth): melhorar organização da lógica de negócio

--- app/Http/Middleware/EnsurePasswordIsChanged.php
+++ app/Http/Middleware/EnsurePasswordIsChanged.php
@@ -10,20 +10,27 @@ use Symfony\Component\HttpFoundation\Response;
 class EnsurePasswordIsChanged
 {
     /**
+     * The names of the routes that are allowed to be accessed.
+     *
+     * @var array<int, string>
+     */
+    protected const ALLOWED_ROUTES = [
+        'change-password',
+        'logout',
+    ];
+
+    /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @return \Illuminate\Http\Response
      */
-
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check()) {
-            if (Auth::user()->first_login) {
-                if (!$request->routeIs('change-password') && !$request->routeIs('logout')) {
-                    return redirect()->route('login');
-                }
+        if (Auth::check() && Auth::user()->first_login) {
+            if (!in_array($request->route()->getName(), self::ALLOWED_ROUTES)) {
+                return redirect()->route('login');
             }
         }
 
