ed1d01ea71653a50193d0b7feb4e3ee4bbe6adc0
KarolineKS <kkohlsstangherlin@gmail.com>
2025-07-11 10:59:40 -0300

    fix: corrige inicializacao do javascript do bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,16 @@
 import "./bootstrap";
 
-import * as bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-window.bootstrap = bootstrap;
-
+// O auth.js pode ser importado aqui se ele tiver lógicas independentes
+// que não dependem da inicialização de componentes.
 import "./auth.js";
 
+// Inicializa todos os toasts que estiverem na página
 document.addEventListener("DOMContentLoaded", function () {
     const toastElList = [].slice.call(document.querySelectorAll(".toast"));
     const toastList = toastElList.map(function (toastEl) {
-        return bootstrap.Toast.getOrCreateInstance(toastEl);
+        // Usa a instância global do Bootstrap, que é a forma correta
+        return new window.bootstrap.Toast(toastEl);
     });
+    // Mostra cada toast.
     toastList.forEach((toast) => toast.show());
 });
