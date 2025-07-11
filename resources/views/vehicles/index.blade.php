e08f3f0ccce48286293c3b686ee52c3d4f21a739
KarolineKS <kkohlsstangherlin@gmail.com>
2025-07-11 16:06:32 -0300

    feat: adicionar ícones ao dropdown de ações

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -7,52 +7,43 @@
 @section('search-placeholder', 'Pesquisar veículo')
 
 @section('content')
-<div class="table-responsive">
-    <table class="table table-custom">
-        <thead>
-            <tr>
-                <th>Nome de Identificação</th>
-                <th>Prefixo</th>
-                <th>Placa</th>
-                <th>Modelo</th>
-                <th>Chassi</th>
-                <th>Tipo de Veículo</th>
-                <th>Capacidade</th>
-                <th>Ano</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($vehicles as $vehicle)
-            <tr>
-                <td>{{ $vehicle->identification_name }}</td>
-                <td>{{ $vehicle->prefix}}</td>
-                <td>{{ $vehicle->license_plate }}</td>
-                <td>{{ $vehicle->model }}</td>
-                <td>{{ $vehicle->chassis }}</td>
-                <td>{{ $vehicle->vehicle_type }}</td>
-                <td>{{ $vehicle->capacity }}</td>
-                <td>{{ $vehicle->year }}</td>
-                <td>
-                    <div class="dropdown">
-                        <button class="btn btn-light" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <x-icon name="three-dots-vertical" />
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Editar veículo</a></li>
-                            <li><a class="dropdown-item" href="#">Deletar veículo</a></li>
-                        </ul>
-                    </div>
-                </td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="8" class="text-center">Nenhum veículo cadastrado.</td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
-</div>
+<x-table>
+    <x-slot name="head">
+        <th>Prefixo</th>
+        <th>Placa</th>
+        <th>Modelo</th>
+        <th>Chassi</th>
+        <th>Tipo de Veículo</th>
+        <th>Capacidade</th>
+        <th>Ano</th>
+        <th>Ações</th>
+    </x-slot>
+
+    <x-slot name="body">
+        @forelse ($vehicles as $vehicle)
+        <tr>
+            <td>{{ $vehicle->prefix }}</td>
+            <td>{{ $vehicle->license_plate }}</td>
+            <td>{{ $vehicle->model }}</td>
+            <td>{{ $vehicle->chassis }}</td>
+            <td>{{ $vehicle->vehicle_type }}</td>
+            <td>{{ $vehicle->capacity }}</td>
+            <td>{{ $vehicle->year }}</td>
+            <td>
+                <x-actions-dropdown
+                    :actions="[
+                        ['url' => '#', 'label' => 'Editar veículo', 'icon' => 'edit'],
+                        ['url' => '#', 'label' => 'Deletar veículo', 'icon' => 'delete']
+                    ]" />
+            </td>
+        </tr>
+        @empty
+        <tr>
+            <td colspan="9" class="text-center">Nenhum veículo cadastrado.</td>
+        </tr>
+        @endforelse
+    </x-slot>
+</x-table>
 
 @include('vehicles.partials.form-offcanvas')
 @endsection
