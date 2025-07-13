94a2cd3030dba4c2a65bc4f001acdb2efa4552eb
KarolineKS <kkohlsstangherlin@gmail.com>
2025-07-13 18:16:19 -0300

    fix: corrigir typo no componente toast

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -6,6 +6,18 @@ $config = [
 'title' => 'Sucesso!',
 'icon' => 'toast-success',
 ],
+'danger' => [
+'title' => 'Erro!',
+'icon' => 'exclamation-triangle',
+],
+'warning' => [
+'title' => 'Atenção!',
+'icon' => 'exclamation-triangle',
+],
+'info' => [
+'title' => 'Informação',
+'icon' => 'info-circle',
+],
 ];
 
 $title = $config[$type]['title'] ?? '';
@@ -17,7 +29,9 @@ $iconName = $config[$type]['icon'] ?? '';
     <div class="toast-header">
         <x-icon :name="$iconName" class="toast-icon" />
         <strong class="me-auto">{{ $title }}</strong>
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close">
+            <x-icon name="x" />
+        </button>
     </div>
     <div class="toast-body">
         {{ $message }}
